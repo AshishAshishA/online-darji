a00f411b4e557b01d08d41b8ceb593fe5e91b5df
Helpless Owner <ashishkr9214jmp@gmail.com>
2025-01-11 01:13:31 +0530

    css changed a bit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,7 +9,6 @@ import { Link } from "react-router-dom";
 import { selectedClothesState, BASE_URL } from "../state/state";
 import { FaRegHeart } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
-
 import { useNavigate } from "react-router-dom";
 
 function Home() {
@@ -21,7 +20,7 @@ function Home() {
   const navigate = useNavigate();
 
   const handleAddClothes = (clothItem) => {
-    if (loginStatus == true) {
+    if (loginStatus === true) {
       setSelectedClothesState((prevState) => {
         if (prevState.indexOf(clothItem) === -1) {
           return [...prevState, clothItem];
@@ -32,7 +31,6 @@ function Home() {
     } else {
       navigate("/login");
     }
-    // console.log(selectedClothes);
   };
 
   useEffect(() => {
@@ -40,7 +38,6 @@ function Home() {
       try {
         const response = await axios.get(`${BASE_URL}/clothes/`);
         setClothesList(response.data);
-        // console.log(response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -50,39 +47,44 @@ function Home() {
   }, []);
 
   return (
-    <div className="flex flex-col min-h-screen w-full">
+    <div className="flex flex-col w-full min-h-screen">
       <Navbar />
-      <div className="flex items-center justify-center">
-        <button className="bg-pink-600 border-2 rounded-md p-1 mt-2 text-white">
-          {loginStatus == true ? (
+      <div className="flex items-center justify-center py-2">
+        <button className="bg-pink-600 border-2 rounded-md p-2 text-white">
+          {loginStatus === true ? (
             <Link to="/book">Book Now</Link>
           ) : (
             <Link to="/login">Book Now</Link>
           )}
         </button>
       </div>
-      <div className="columns-2 mt-3 ml-2 mr-2">
-        <div>
-          {clothesList.map((item) => (
-            <div
-              key={item.id}
-              className="flex flex-col items-center justify-center p-1 border-2 border-black mb-2 h-[25vh]"
-            >
-              <img src={item.photo} className="w-[40vw] h-[10vh]" alt="image" />
-              <p>{item.type}</p>
-              <p>{item.price}</p>
-              <FaHeart
-                onClick={() => handleAddClothes(item)}
-                className={
-                  selectedClothes.indexOf(item) == -1
-                    ? "text-gray-300"
-                    : "text-pink-500"
-                }
-              />
-            </div>
-          ))}
-        </div>
+
+      {/* Clothes List Section - Two Columns */}
+      <div className="grid grid-cols-2 gap-4 mt-4 px-4">
+        {clothesList.map((item) => (
+          <div
+            key={item.id}
+            className="flex flex-col items-center justify-center p-2 border-2 border-black rounded-md w-[100px] h-auto"
+          >
+            <img
+              src={item.photo}
+              className="w-full h-[70px] object-cover rounded-md mb-2"
+              alt="clothes"
+            />
+            <p className="text-center text-sm font-semibold">{item.type}</p>
+            <p className="text-center text-sm text-gray-700">{item.price}</p>
+            <FaHeart
+              onClick={() => handleAddClothes(item)}
+              className={
+                selectedClothes.indexOf(item) === -1
+                  ? "text-gray-300"
+                  : "text-pink-500"
+              }
+            />
+          </div>
+        ))}
       </div>
+        <br />
       <Footer />
     </div>
   );
